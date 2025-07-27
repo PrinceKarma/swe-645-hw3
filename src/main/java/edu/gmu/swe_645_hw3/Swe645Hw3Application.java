1e0b23ce19f3ad4fbe1dc15412d7e140f8bfdc42
Amir Ahmed <topahmed1@gmail.com>
2025-07-27 15:28:14 -0400

    Intial setup

---
+++ src/main/java/edu/gmu/swe_645_hw3/Swe645Hw3Application.java
@@ -0,0 +1,13 @@
+package edu.gmu.swe_645_hw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swe645Hw3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swe645Hw3Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/gmu/swe_645_hw3/Swe645Hw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gmu.swe_645_hw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swe645Hw3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
